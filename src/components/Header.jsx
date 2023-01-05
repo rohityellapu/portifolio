19aa1580c089d7385bcc328e8aeffefc65802684
rohityellapu <rohithyellapu24@gmail.com>
2023-01-05 21:30:43 +0530

    added logo to page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import '../static/Header.css';
 function Header() {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const element = document.getElementById(event.target.getAttribute('href').substring(1));
+    element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
   return (
-      <>
-          <header>
-              <nav>
-                  <ul className='nav-links flex flex-row'>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
-              </ul></nav>
-          </header>
-      </>
+    <>
+      <header>
+        <nav className='flex flex-row space-between'>
+          <div className="logo">
+            <img src={ require('../images/logo.png') } height='120rem' width='120rem' alt="ry" />
+          </div>
+          <ul className='nav-links flex flex-row'>
+            <li><a href="" onClick={ handleClick }>About</a></li>
+            <li> <a href="#skills" onClick={ handleClick }>Skills</a></li>
+            <li><a href="#contact" onClick={ handleClick }>Projects</a></li>
+            <li><a href="#contacts" onClick={ handleClick }>Contact</a></li>
+          </ul></nav>
+      </header>
+    </>
   )
 }
 
