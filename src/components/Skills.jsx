b0ac9b6a81a58c019ab946460794c4a5c4ba6298
rohityellapu <rohithyellapu24@gmail.com>
2023-01-15 18:32:30 +0530

    updated c++ skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import '../static/Skills.css'
 import Skill from './Skill'
 function Skills() {
@@ -8,55 +8,55 @@ function Skills() {
     useEffect(() => {
         const handleScroll = () => {
             setIsScrolled(window.scrollY > 50);
-           
+            
         };
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
-  return (
-      <div id='skills' className="skills">
-          <h1>Technologies I use currently or used previosly</h1>
-       
-          <ul className={ `box progress ${isScrolled ? 'show' : ''}` }>
-              { skills.map(skill => {
-                  return <Skill key={skill.name} name={skill.name } percent={skill.percent} />
-              })}
-               
-          </ul>
- 
-          <svg width="0" height="0">
-              <defs>
-                  <linearGradient id="cl1" gradientUnits="objectBoundingBox" x1="0" y1="0" x2="1" y2="1">
-                      <stop stopColor="#618099" />
-                      <stop offset="100%" stopColor="#8e6677" />
-                  </linearGradient>
-                  <linearGradient id="cl2" gradientUnits="objectBoundingBox" x1="0" y1="0" x2="0" y2="1">
-                      <stop stopColor="#8e6677" />
-                      <stop offset="100%" stopColor="#9b5e67" />
-                  </linearGradient>
-                  <linearGradient id="cl3" gradientUnits="objectBoundingBox" x1="1" y1="0" x2="0" y2="1">
-                      <stop stopColor="#9b5e67" />
-                      <stop offset="100%" stopColor="#9c787a" />
-                  </linearGradient>
-                  <linearGradient id="cl4" gradientUnits="objectBoundingBox" x1="1" y1="1" x2="0" y2="0">
-                      <stop stopColor="#9c787a" />
-                      <stop offset="100%" stopColor="#817a94" />
-                  </linearGradient>
-                  <linearGradient id="cl5" gradientUnits="objectBoundingBox" x1="0" y1="1" x2="0" y2="0">
-                      <stop stopColor="#817a94" />
-                      <stop offset="100%" stopColor="#498a98" />
-                  </linearGradient>
-                  <linearGradient id="cl6" gradientUnits="objectBoundingBox" x1="0" y1="1" x2="1" y2="0">
-                      <stop stopColor="#498a98" />
-                      <stop offset="100%" stopColor="#618099" />
-                  </linearGradient>
-              </defs>
-          </svg>
+    return (
+        <div id='skills' className="skills">
+            <h1 style={ { fontSize: '3rem' } }>Technologies I use currently or used previosly</h1>
 
-    </div>
-  )
+            <ul className={ `box progress ${isScrolled ? 'show' : ''}` }>
+                { skills.map(skill => {
+                    return <Skill key={ skill.name } name={ skill.name } percent={ skill.percent } />
+                }) }
+
+            </ul>
+
+            <svg width="0" height="0">
+                <defs>
+                    <linearGradient id="cl1" gradientUnits="objectBoundingBox" x1="0" y1="0" x2="1" y2="1">
+                        <stop stopColor="#618099" />
+                        <stop offset="100%" stopColor="#8e6677" />
+                    </linearGradient>
+                    <linearGradient id="cl2" gradientUnits="objectBoundingBox" x1="0" y1="0" x2="0" y2="1">
+                        <stop stopColor="#8e6677" />
+                        <stop offset="100%" stopColor="#9b5e67" />
+                    </linearGradient>
+                    <linearGradient id="cl3" gradientUnits="objectBoundingBox" x1="1" y1="0" x2="0" y2="1">
+                        <stop stopColor="#9b5e67" />
+                        <stop offset="100%" stopColor="#9c787a" />
+                    </linearGradient>
+                    <linearGradient id="cl4" gradientUnits="objectBoundingBox" x1="1" y1="1" x2="0" y2="0">
+                        <stop stopColor="#9c787a" />
+                        <stop offset="100%" stopColor="#817a94" />
+                    </linearGradient>
+                    <linearGradient id="cl5" gradientUnits="objectBoundingBox" x1="0" y1="1" x2="0" y2="0">
+                        <stop stopColor="#817a94" />
+                        <stop offset="100%" stopColor="#498a98" />
+                    </linearGradient>
+                    <linearGradient id="cl6" gradientUnits="objectBoundingBox" x1="0" y1="1" x2="1" y2="0">
+                        <stop stopColor="#498a98" />
+                        <stop offset="100%" stopColor="#618099" />
+                    </linearGradient>
+                </defs>
+            </svg>
+
+        </div>
+    )
 }
 
 
@@ -107,11 +107,11 @@ const skills = [
     },
     {
         name: 'C++',
-        percent: 81,
+        percent: 89,
     },
     {
         name: 'Java',
-        percent: 86,
+        percent: 81,
     },
     {
         name: 'Python',
