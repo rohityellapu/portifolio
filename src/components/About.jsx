361dec7ca2956a6354f6fee8be7c02d64a3cf9a1
rohityellapu <rohithyellapu24@gmail.com>
2023-01-15 18:33:00 +0530

    updated width and fontsize

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function About() {
 
     return (
-        <div  className='aboutx flex flex-column items-center'>
-            <h2>
+        <div id='about' className='aboutx flex flex-column items-center'>
+            <h2 style={ { fontSize: '3rem' } }>
                 My Story
             </h2>
-            <p style={ { width:'50%', letterSpacing: '1px', textAlign: 'center'}}>I always wondered about knowing the point and purpose of study since my childhood.
+            <p style={ { width:'80%', letterSpacing: '1px', textAlign: 'center'}}>I always wondered about knowing the point and purpose of study since my childhood.
                 As I grew up, the study of Internet and Technology somehow constantly impressed me.  As a self-taught learner, I started learning to code online as it helped me to find my niche.
             
             </p>
-            <p style={ { width: '60%', letterSpacing: '1px', textAlign: 'center' } }>
+            <p style={ { width: '70%', letterSpacing: '1px', textAlign: 'center' } }>
                 Being a Math geek, I loved solving problems which involve logical and analytical thinking and made me
                 great at understanding Data Structures and Algorithms. While digging more and more into tech world
                 I get to know my real passion and decided to persue my career in Web Programming. 
@@ -20,7 +20,7 @@ function About() {
                 taught by David J. Malan. It helped me in acing at fundamentals of programming and diving deep into coding pool. To fill the gaps in my Web Developing career I joined a Full Stack Development Bootcamp on 10x 
                 Academy. They helped me to work on real-world projects with big tech industry-level experts.
             </p>
-            <p style={ { width: '50%', letterSpacing: '1px', textAlign: 'center' } }>
+            <p style={ { width: '60%', letterSpacing: '1px', textAlign: 'center' } }>
                 Currently, I'm looking for a Junior Web Developer position where I can contribute my compelling skills 
                 and enhance my career in Web programming. I  recently worked on MERN Stack Development and built industry level
                  team and personal projects. 
