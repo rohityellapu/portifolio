e9f3158975ce61e753cab8564771849d9d8399b8
rohityellapu <rohithyellapu24@gmail.com>
2023-02-12 13:24:34 +0530

    debugging scrolling behavior

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -8,8 +8,6 @@ function Links() {
     window.addEventListener('scroll', () => setOp(window.scrollY / 550))
     const handleClick = (event) => {
         event.preventDefault();
-        event.stopPropagation();
-
         const element = document.getElementById(event.target.getAttribute('href').substring(1));
 
         element.scrollIntoView({ behavior: 'smooth', block: 'start' });
@@ -20,11 +18,11 @@ function Links() {
         } }>
 
             <ul className='aside-links flex flex-row justify-center shadow-md shadow-amber-300 gap-4 bg-[#241304ba] p-2 px-4 rounded-full' >
-                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#head"><HiOutlineHome className="h-10 w-10" href="#head" onClick={ handleClick } /></a></li>
-                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#about"><FaUserGraduate className="h-10 w-10" href="#about" onClick={ handleClick } /></a></li>
-                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#skills"><AiOutlineCode className="h-10 w-10" href="#skills" onClick={ handleClick } /></a></li>
-                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#project"><FaProjectDiagram className="h-10 w-10" href="#project" onClick={ handleClick } /></a></li>
-                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#contacts"><HiChatAlt2 className="h-10 w-10" href="#contact" onClick={ handleClick } /></a></li>
+                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#head" onClick={ handleClick }><HiOutlineHome className="h-10 w-10" href="#head" /></a></li>
+                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#about" onClick={ handleClick }><FaUserGraduate className="h-10 w-10" href="#about" /></a></li>
+                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#skills" onClick={ handleClick }><AiOutlineCode className="h-10 w-10" href="#skills" /></a></li>
+                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#project" onClick={ handleClick }><FaProjectDiagram className="h-10 w-10" href="#project" /></a></li>
+                <li><a className='nav-link rounded-full hover:scale-125 hover:text-black' href="#contacts" onClick={ handleClick }><HiChatAlt2 className="h-10 w-10" href="#contact" /></a></li>
             </ul>
         </div>
 
