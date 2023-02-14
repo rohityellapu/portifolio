5c6911a8f13550c6996bd4c86a0fc2839c86ff5d
rohityellapu <rohithyellapu24@gmail.com>
2023-02-14 11:06:00 +0530

    added some more projects and configured links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,19 +27,49 @@ function Projects() {
 
 const projects = [
     {
+        name: "Finance",
+        description: "Rohit's Finance, a web app via which you can manage portfolios of stocks. Not only will this tool allow you to check real stocks’ actual prices and portfolios’ values, it will also let you buy (okay, “buy”) and sell (okay, “sell”) stocks by querying IEX for stocks’ prices.",
+        imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1676349323/Projects/finance_n4fora.png',
+        liveLink: 'https://finance-rohit.onrender.com',
+        gitHubLink: "https://github.com/rohityellapu/finance"
+    },
+    {
+        name: "YelpCamp",
+        description: "Jump right in and explore our many campgrounds. Feel free to share some of your own and comment on others. It'll let camp lovers know the best spots to go relaxing.",
+        imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1676349319/Projects/yelpcamp_yju0m1.png',
+        liveLink: 'https://rohits-yelpcamp.onrender.com',
+        gitHubLink: "https://github.com/rohityellapu/yelpCamp"
+    },
+    {
+        name: "Instagram Clone",
+        description: "Clone of Instagram where we can post feed, see others posts with Infinite Scrolling, Built with React with fully responsive design. Looking forward to add new features in this app that may fill gaps in Facebook's Instagram.",
+        imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1676349301/Projects/instagram_arsyi7.png',
+        liveLink: 'https://instagram-byrohit.vercel.app',
+        gitHubLink: "https://github.com/rohityellapu/instaclone-V2"
+    },
+
+    {
+        name: "Task Manager",
+        description: "Add your daily tasks or todos with ease. Manage your activities quickly completing the tasks one by one. User can also pause his activity and start another activity, thus completing the tasks at user's own pace.",
+        imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1676349330/Projects/taskManager_j82evf.png',
+        liveLink: 'https://rohits-todo.onrender.com',
+        gitHubLink: "https://github.com/rohityellapu/task-manager"
+    },
+    {
+        name: "Ping Pong Game",
+        description: "A Famous Two Player Ping Pong Game with keyboard inputs as controllers, Bet your friends could beat you on this, built with Scratch MIT Edu.",
+        imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1676349335/Projects/Web_capture_13-2-2023_132758_scratch.mit.edu_rqem4k.jpg',
+        liveLink: 'https://scratch.mit.edu/projects/762417318/fullscreen/',
+        gitHubLink: "https://github.com/rohityellapu/CS50x/tree/main/Week%200%20Scratch"
+    },
+    {
         name: "Real Estate Catalog",
         description: "An Estate Catalog where Real estate agents can add their property making them easy to analyse and compare the number of properties. It'll make adding and describing your properties and with a proper maintainance.",
         imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1672898503/Projects/Screenshot_2023-01-05_11.30.46_qzg3vk.png',
         liveLink: 'https://estate-catalog-by-rohit-chava-harish.onrender.com',
         gitHubLink: "https://github.com/rohityellapu/RealEstate-Client"
     },
-    {
-        name: "YelpCamp",
-        description: "Jump right in and explore our many campgrounds. Feel free to share some of your own and comment on others. It'll let camp lovers know the best spots to go relaxing.",
-        imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1672900122/Projects/Screenshot_2023-01-05_11.56.35_rqj4ys.png',
-        liveLink: 'https://rohits-yelpcamp.onrender.com',
-        gitHubLink: "https://github.com/rohityellapu/yelpCamp"
-    },
+
     {
         name: "Deliverio",
         description: "A Food Delivery mobile application built on React-Native with help of Expo Go compatible on both Android and iOS. Acknowledge the logic behind food delivery applications.",
@@ -48,12 +78,13 @@ const projects = [
         gitHubLink: "https://github.com/rohityellapu/deliverio"
     },
     {
-        name: "CS50 Finance",
-        description: "C$50 Finance, a web app via which you can manage portfolios of stocks. Not only will this tool allow you to check real stocks’ actual prices and portfolios’ values, it will also let you buy (okay, “buy”) and sell (okay, “sell”) stocks by querying IEX for stocks’ prices.",
-        imageUrl: 'https://cs50.harvard.edu/x/2022/psets/9/finance/finance.png',
-        liveLink: 'https://finance.cs50.net/',
-        gitHubLink: "https://github.com/rohityellapu/finance"
+        name: "Portfolio",
+        description: "Magazine of all my Frontend, Backend, Full Stack projects and applications using various tech stacks. Connect if you have any brilliant app ideas that needs to be built to bring into world.",
+        imageUrl: 'https://res.cloudinary.com/dovkoy31j/image/upload/v1676349313/Projects/portfolio_yp9sy7.png',
+        liveLink: 'https://rohityellapu-portifolio.vercel.app/',
+        gitHubLink: "https://github.com/rohityellapu/portifolio"
     },
+
 ]
 
 export default Projects
