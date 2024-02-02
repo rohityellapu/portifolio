9a2c7b065d0478744caa70bf69e764ad581aa14f
Rohit Yellapu <100713605+rohityellapu@users.noreply.github.com>
2024-02-02 23:58:09 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,15 +15,15 @@ function About() {
                 Being a Math geek, I loved solving problems which involve logical and analytical thinking and made me
                 great at understanding Data Structures and Algorithms. While digging more and more into tech world
                 I get to know my real passion and decided to persue my career in Web Programming.
-                Due to my personal, financial problems and false influences I couldn't persue a CS degree, but my passion for coding never stopped.
+                My passion for coding never stopped.
                 For a kick-start I enrolled into a professional program CS50x Introduction to Computer Science from Harvard University
                 taught by David J. Malan. It helped me in acing at fundamentals of programming and diving deep into coding pool. To fill the gaps in my Web Developing career I joined a Full Stack Development Bootcamp on 10x
                 Academy. They helped me to work on real-world projects with big tech industry-level experts.
             </p>
             <p >
-                Currently, I'm looking for a Junior Web Developer position where I can contribute my compelling skills
-                and enhance my career in Web programming. I  recently worked on MERN Stack Development and built industry level
-                team and personal projects.
+                Currently, I am working as a Full Stack Developer in OpsVeda Asia Private Limited, enhancing my knowledge where businesses marries technology. Constantly contributing my compelling skills
+                and enhance my career in Web programming. I have worked with industry level
+                team at enterprises level projects. My passion of stretching myself in technologies never stops and always be looking for new oppurtunities and new challenges.
             </p>
         </div>
     )
